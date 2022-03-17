9e27ad2811694ec71ba1f8c927d9451e0b912b12
Utkarsh10 <utkarshnagar2000@gmail.com>
2022-03-17 16:35:29 +0530

    Chakra UI fixed

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router} from 'react-router-dom'
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import {BrowserRouter as Router} from "react-router-dom";
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk";
 import App from "./App";
+import {ChakraProvider} from "@chakra-ui/react";
 import "./styles/index.scss";
 
 import reducers from "./reducers";
@@ -14,10 +15,11 @@ const store = createStore(reducers, compose(applyMiddleware(thunk)));
 ReactDOM.render(
   // Provider connects the store (global states) to the entire App
   <Provider store={store}>
-    <Router>
+    <ChakraProvider>
+      <Router>
         <App />
-    </Router>
-  
+      </Router>
+    </ChakraProvider>
   </Provider>,
   document.getElementById("root")
 );
