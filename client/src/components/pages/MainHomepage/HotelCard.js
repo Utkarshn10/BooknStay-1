d243cf0dd60865a4feab6aa3e13733f9fa8f3e6a
Utkarsh10 <utkarshnagar2000@gmail.com>
2022-03-21 14:29:29 +0530

    temp

--- client/src/components/pages/MainHomepage/HotelCard.js
+++ client/src/components/pages/MainHomepage/HotelCard.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
-import Rating from '../../Rating'
-import ReactStars from 'react-rating-stars-component'
-
+import {Card, ListGroup, ListGroupItem} from "react-bootstrap";
+import Rating from "../../Rating";
+import ReactStars from "react-rating-stars-component";
 
 export default function HotelCard() {
   return (
     <div>
-      
       {/* <Rating/> */}
-      
-      <Card style={{ width: "20rem" }}>
+
+      <Card style={{width: "20rem"}}>
         <Card.Img
           variant="top"
           src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
         />
         <Card.Body>
-        <Card.Title>Ananya's Hotel </Card.Title>
-        
+          <Card.Title>Ananya's Hotel </Card.Title>
 
           <Card.Text>Lorem, ipsum dolor.</Card.Text>
-          <Card.Title> <ReactStars count={5} value={4.5} size={50} edit={false}  /> </Card.Title>
+          <Card.Title>
+            {" "}
+            <ReactStars count={5} value={4.5} size={50} edit={false} />{" "}
+          </Card.Title>
         </Card.Body>
         <ListGroup className="list-group-flush">
           <ListGroupItem>Rs 15000/night - Free Cancellation</ListGroupItem>
@@ -30,7 +30,7 @@ export default function HotelCard() {
         </ListGroup>
 
         <Card.Body>
-          <Card.Link href="#"> 4.6  </Card.Link>
+          <Card.Link href="#"> 4.6 </Card.Link>
           <Card.Link href="#">Read More..</Card.Link>
         </Card.Body>
       </Card>
